cbe3fe2e460c10aa51a3d5133e0a1e2c1db405f2
Jeremy Hanson <rockbasser88@hotmail.com>
2023-01-13 13:59:05 -0600

    added object data for each photo

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,9 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    {id: 2, path: 'images/beaver.jpg', description: `A beaver sitting on it's tail, looking at the camera.`, likes: 0},
+    {id: 3, path: 'images/cheetah.jpg', description: 'A cheetah standing in a field, looking at the camera.', likes: 0},
+    {id: 4, path: 'images/polar-bear-faces.jpg', description: 'Two polar bears in the snow, getting close to the camera.', likes: 0},
+    {id: 5, path: 'images/snow-skunk.jpg', description: 'A striped skunk walking in the snow.', likes: 0}
 ];
 
 module.exports = galleryItems;
